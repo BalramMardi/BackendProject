ea946a9a9e8a73da7e27eea1014c3cb550c9d422
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2024-11-28 18:44:44 +0530

    281124v

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use("/auth", authRoutes);
 app.use("/resource", resourceRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
